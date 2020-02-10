5d61bcb0270f7d9ce38dcd3fc744c5b31e6affbf
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-10 15:24:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+ hash = {}
+ array.each_with_index {|d, index|
+    index += 1
+    hash[index] = d }
+ puts hash
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  new_array = array.map { |call| 
+    "#{call.capitalize}!" }
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
+  array.any? {|word| word.length > 4
+  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|snacks|
+    if cheese_types.include? (snacks)
+      snacks
+    end 
+  }
 end
